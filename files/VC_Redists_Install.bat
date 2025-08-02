45ed961dd3e17c34b2fc77fe074490638dfd98f9
poke <96556554+zpok3@users.noreply.github.com>
2025-08-01 19:31:02 -0700

    move to folder

---
+++ files/VC_Redists_Install.bat
@@ -0,0 +1,1 @@
+winget install Microsoft.VCRedist.2005.x64 Microsoft.VCRedist.2005.x86 Microsoft.VCRedist.2008.x64 Microsoft.VCRedist.2008.x86 Microsoft.VCRedist.2010.x64 Microsoft.VCRedist.2010.x86 Microsoft.VCRedist.2012.x64 Microsoft.VCRedist.2012.x86 Microsoft.VCRedist.2013.x64 Microsoft.VCRedist.2013.x86 Microsoft.VCRedist.2015+.x64 Microsoft.VCRedist.2015+.x86
